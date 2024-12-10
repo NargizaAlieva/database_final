2437d7b59f739e5a00d9b10decbe02e21f172582
git-aidai-kydykbekova <aidai.kydykbekova@alatoo.edu.kg>
2024-12-10 22:19:49 +0600

    added Student and Feedback Entity

---
+++ database_final/src/main/java/org/example/database_final/entity/Student.java
@@ -0,0 +1,78 @@
+package org.example.database_final.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "registration_date")
+    private LocalDate registrationDate;
+
+    public Student() {}
+
+    @PrePersist
+    private void prePersist() {
+        registrationDate = LocalDate.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Student setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Student setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Student setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+        return this;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public Student setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+        return this;
+    }
+}
+
